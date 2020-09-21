95bcb6440ccc7b08a9f3cfbcc18438f5f4c9436d
adityaviren <adityaviren@gmail.com>
2020-09-21 12:43:51 +0530

    Create

---
+++ src/basic/learncreatingrepofromlocal.java
@@ -0,0 +1,19 @@
+package basic;
+import java.util.Scanner;
+public class learncreatingrepofromlocal {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("1st num");
+		int a=sc.nextInt();
+		System.out.println("2nd num");
+		int b=sc.nextInt();
+		add(a,b);
+	}
+	private static void add(int a,int b) {
+		long sum=a+b;
+		System.out.println("Sum is "+sum);
+	}
+
+}
